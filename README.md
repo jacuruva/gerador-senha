ee8cdb6a6413951815de0ad9b72ee967458d53aa
João Pedro <joaojacuruba@gmail.com>
2022-08-17 22:08:47 -0300

    v1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# GERADOR DE SENHA
+
+- Gerador de senha com caracteres aleatórios
+- Feito por Cururu#6940
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import formGeraSenha from "./assets/modules/formGeraSenha";
+import "./assets/css/style.css";
+formGeraSenha();
